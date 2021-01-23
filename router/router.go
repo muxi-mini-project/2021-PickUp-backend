02c0efe19fe7d262e595d3f745899dd3e5974975
zxy-lgtm <1563925535@qq.com>
2021-01-23 15:33:10 +0800

    3

--- router/router.go
+++ router/router.go
@@ -4,30 +4,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func router() {
+func Router() {
 	r := gin.Default()
 
 	//注册
-	r.POST("/users", addNewUsers)
+	r.POST("/users", AddNewUsers)
 
 	//登录
-	r.POST("/users/login", loginUser)
+	r.POST("/users/login", LoginUser)
 
 	//用户主页及其信息修改
 	r.GET("/users/{user_id}")
-	r.PUT("/users/{user_id}", updateUser)
+	r.PUT("/users/{user_id}", UpdateUser)
 
 	//用户修改密码
-	r.PUT("/users/password", updatePassword)
+	r.PUT("/users/password", UpdatePassword)
 
 	//用户评价
-	r.PUT("/users/comment/{comment_id}", usersComment)
+	r.PUT("/users/comment/{comment_id}", UsersComment)
 	r.GET("/users/comment/{comment_id}")
 
 	//司机订单
-	r.POST("/driver", addDriverRequirement)
+	r.POST("/driver", AddDriverRequirement)
 	r.GET("/driver")
-	r.DELETE("/driver", deleteDriverRequirement)
+	r.DELETE("/driver", DeleteDriverRequirement)
 
 	//司机确认(注意乘客先确认,司机后确认)
 	r.POST("​/driver​/confirm​/{confirm_id}", driverConfirm)
